c39ee022956dc30f65ff141846a3722d551699f2
erfan <erfan>
2018-02-10 21:17:14 +0330

    forth

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path')
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 //entry ->output
-module.exports={
-    entry:'./src/app.js',
+
+
+module.exports=(env)=>{
+    return {
+        entry:'./src/app.js',
     output:{
         path: path.join(__dirname, 'public'),
         filename:'bundle.js'
@@ -15,17 +18,33 @@ module.exports={
         },
         {
             test: /\.s?css$/,
-            use: [
-                { loader: "style-loader" },
-                { loader: "css-loader" },
-                { loader: "sass-loader" }
-            ]
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [{
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }
+                    , {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }]
+            })
         }
     ]
-    },
-    devtool:'cheap-module-eval-source-map',
-    devServer:{
+        }, plugins: [
+            new ExtractTextPlugin('style.css')
+            //if you want to pass in options, you can do so:
+            //new ExtractTextPlugin({
+            //  filename: 'style.css'
+            //})
+        ],
+        devtool: (env === "production" ? 'source-map' :'inline-source-map'),
+         devServer:{
         contentBase:path.join(__dirname,'public'),
         historyApiFallback:true
     }
-};
+}};
