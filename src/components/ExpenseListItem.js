42ce8715df2130f991516553d626a3930f55a403
erfan <erfan>
2018-02-10 23:25:07 +0330

    numeral

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { expensesRemove } from '../actions/expenses'
 import { NavLink } from 'react-router-dom';
 import moment from 'moment'
+import numeral from 'numeral'
 export const Item = ({dispatch , id , description, amount, note, createAt})=>(
     <div>
         <h3>
@@ -10,7 +11,7 @@ export const Item = ({dispatch , id , description, amount, note, createAt})=>(
              <NavLink key={id} to={'/edit/' + id } ><button>Edit</button></NavLink>
               
              (description  : {description && description})
-             (amount       : {amount && amount})          
+             (amount       : {amount && numeral(amount).format('$0,0.00')})          
              ( note         : {note && note})         
              (ceateAt      : {createAt && moment(createAt).format('YYYY MM DD')  })    
         </h3>
